53b3ee2b771e01f150c63883d4a79c66bb4f71c9
Daniel Morris <danieljohnmorris@gmail.com>
2025-12-22 22:27:23 +0000

    feat: enable hybrid line numbers (absolute + relative)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.number = true         -- Show absolute line number on current line
+vim.opt.relativenumber = true -- Show relative line numbers on other lines

7a6507c6e0e4a8a450d4df44cfcbabe2bad62721
Daniel Morris <danieljohnmorris@gmail.com>
2025-12-23 18:16:02 +0000

    fix: remove absolute line number column from statuscol

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -8,15 +8,6 @@ return {
         { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
         { text = { "%s" }, click = "v:lua.ScSa" },
         { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-        {
-          text = {
-            function(args)
-              return string.format("%3d ", args.lnum)
-            end,
-          },
-          condition = { true },
-          click = "v:lua.ScLa",
-        },
       },
     })
   end,
